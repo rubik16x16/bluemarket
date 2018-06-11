7dd1fc25b60d2491351ddafb102a00b311b39fd6
rubik16x16 <drt_mike@hotmail.com>
2018-06-11 18:14:58 -0400

    Creando migraciones de productos y operaciones

---
+++ database/migrations/2018_06_09_214843_create_productos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->string('nombre', 30);
+            $table->integer('cantidad');
+            $table->float('precio');
+            $table->boolean('estado');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
---
+++ database/migrations/2018_06_11_210745_create_table_operaciones.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableOperaciones extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operaciones', function (Blueprint $table) {
+
+            $table->increments('id');
+            $table->integer('comprador_id')->references('id')->on('usuarios')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('vendedor_id')->references('id')->on('usuarios')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('producto_id')->references('id')->on('productos')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('cantidad');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operaciones');
+    }
+}
