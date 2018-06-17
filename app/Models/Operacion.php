4363f975330379010de9c39e234724b1499a4b23
rubik16x16 <drt_mike@hotmail.com>
2018-06-17 17:48:52 -0400

    Agregar metodos de relacion a los modelos "Operacion" y "Usuario"

--- app/Models/Operacion.php
+++ app/Models/Operacion.php
@@ -9,4 +9,16 @@ class Operacion extends Model{
   protected $table= 'operaciones';
   protected $fillable= ['comprador_id', 'vendedor_id', 'producto_id', 'cantidad'];
 
+  public function producto(){
+
+    return $this->belongsTo('App\Models\Producto');
+
+  }
+
+  public function total(){
+
+    return $this->producto->precio * $this->cantidad;
+
+  }
+
 }
