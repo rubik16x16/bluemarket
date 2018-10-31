1686e31383013b8f8a1d72a27787918f449962b5
rubik16x16 <drt_mike@hotmail.com>
2018-10-31 12:32:07 -0400

    Instalar bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import 'popper.js';
+import 'bootstrap';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
